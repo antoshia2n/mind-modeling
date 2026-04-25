a2cec496b1138e190a9c381c14121faa27b654ec
antoshia2n <gameister1@gmail.com>
2026-04-25 14:58:46 +0900

    Add files via upload

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,3 @@
-// ← このファイルを各アプリで書き換える
-export const APP_ID   = "my-app";       // Firestore allowedApps で使われるID
-export const APP_NAME = "My App";       // ログイン画面に表示される名前
-
-export const TABS = ["ホーム", "設定"];
+export const APP_ID   = "mm";
+export const APP_NAME = "Mind-Modeling";
+export const TABS = [];
